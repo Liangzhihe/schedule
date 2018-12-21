4c3b21a870970e7fd4882f69d26e162a47fd127e
Liangzhihe <1789211411@qq.com>
2018-12-21 16:11:26 +0800

    temp

--- res/js/index.js
+++ res/js/index.js
@@ -1,43 +1,34 @@
 $(function () {
 
-    function getDate(projectName,buildNumber) {
-        const p = projectName || '中山项目';
-        const b = buildNumber || '#1';
-       
+    function getDate(buildSequence = '#1') {
+        const p = getProjectName() || '中山项目';
+        const b = buildSequence;
+
         axios.get('../../mock/buildDate.json', {
             params: {
                 ProjectName: p,
-                BuildNumber: b
+                BuildSequence: b
             }
         }).then(function (response) {
-            console.log(response.data);
-
-            console.log(response.status);
-            console.log(response.statusText);
-            console.log(response.headers);
-            console.log(response.config);
-
-            const isInit = response.data.isInit;
-            isInit ? randerSchedule(response.data.randerData) : showInitForm();
+            // console.log(response.data);
+            const isInit = response.data.isInit; //是否已经进行过初始化
+            !isInit ? randerSchedule(response.data.randerData) : showInitForm();
         }).catch(function (err) {
             console.log(err);
         })
     }
 
-    getDate();
-
-
-    $('body').on('click', '#buildSelectBtn', function (e) {
-        e.preventDefault();
-        //ajax请求数据
-        var buildNumber = $('#buildNumber').val();
-        //清空画布
-        //获取数据
-        getDate('中山项目', buildNumber);
-    })
+    function getProjectName() {
+        return '中山项目';
+    }
 
     function randerSchedule(data) {
+        //此处先清空画布，然后根据数据生成图表
         console.log(data);
+
+        const sche = new Schedule('schedule', data);
+        sche.init();
+
     }
 
     function showInitForm() {
@@ -45,20 +36,87 @@ $(function () {
         $('#init-form').show();
     }
 
-    function calculate(startDate, template) {
+    function getEndDate(startDate, template) {
+        const endDate = '2020-12-15';
+        return endDate;
+    }
+
+    // 根据模板获取楼层数（实际情况未知）
+    function getBuildLayersByTemplate(template) {
+        return 28;
+    }
+
+    // 根据开始日期及模板生成各进度计划数据
+    function getScheduleListByTemplate(startDate, template) {
+        return [{
+                "color": "#09f",
+                "planName": "砌体工程",
+                "planType": "line",
+                "pointlist": [{
+                        "floor": 1,
+                        "date": "2018-04-13"
+                    },
+                    {
+                        "floor": 2,
+                        "date": "2018-04-21"
+                    }
+                ]
+            },
+            {
+                "color": "#f09",
+                "planName": "主体封顶",
+                "planType": "line",
+                "pointlist": [{
+                        "floor": 1,
+                        "date": "2018-08-13"
+                    },
+                    {
+                        "floor": 2,
+                        "date": "2018-08-28"
+                    }
+                ]
+            }
+        ];
+    }
+
+    function calculate(startDate, template, buildSequence) {
         //根据开始日期及模板生成标准格式数据
-        const data = {};
+        var projectName = getProjectName();
+        const data = {
+            "isInit": true,
+            "randerData": {
+                "startDate": startDate,
+                "endDate": getEndDate(startDate, template),
+                "projectName": projectName,
+                "buildSequence": buildSequence,
+                "buildLayers": getBuildLayersByTemplate(template),
+                "scheduleList": getScheduleListByTemplate(startDate, template)
+            }
+        };
         return data;
     }
 
-    function initSchedule(startDate, template) {
+    function initSchedule(startDate, template, buildSequence) {
         //根据开始日期及模板生成标准格式数据，然后根据数据渲染图表
-        const data = calculate(startDate,template);
+        const start = dateFormat(startDate);
+        const data = calculate(start, template, buildSequence);
         randerSchedule(data);
     }
 
-
-
+    function dateFormat(dateObj) {
+        console.log(dateObj);
+        const year = dateObj.year;
+        let month = dateObj.month;
+        let day = dateObj.date;
+        // if (month < 10) {
+        //     month = '0' + month;
+        // }
+        // if (day < 10) {
+        //     day = '0' + day;
+        // }
+        let date = year + '-' + month + '-' + day;
+        return date;
+    }
 
 
     //时间插件
@@ -71,11 +129,16 @@ $(function () {
         laydate.render({
             elem: '#startDate',
             done: function (value, date) {
-                // console.log(value); //得到日期生成的值，如：2017-08-18
-                // console.log(date); //得到日期时间对象：{year: 2017, month: 8, date: 18, hours: 0, minutes: 0, seconds: 0}
+                //date格式--{year: 2017, month: 8, date: 18, hours: 0, minutes: 0, seconds: 0}
                 startDate = date;
             }
         });
+    });
+
+    $('body').on('change', '#buildSequence', function () {
+        var buildSequence = $(this).val();
+        //获取数据
+        getDate(buildSequence);
     });
 
     $('body').on('click', '#initCanvasBtn', function (e) {
@@ -85,12 +148,13 @@ $(function () {
             return;
         }
         var template = $('#template').val();
-
-        console.log(template, typeof template);
-        initSchedule(startDate, template);
+        var buildSequence = $('#buildSequence').val();
+        // console.log(template, typeof template);
+        initSchedule(startDate, template, buildSequence);
+        $('#init-form').hide();
     });
 
-
-
+    // 进入页面时，获取默认数据（暂定为楼栋#1），生成图表
+    getDate();
 
 });
--- res/js/schedule.js
+++ res/js/schedule.js
@@ -1,22 +1,105 @@
+function Schedule(id, data) {
+    this.id = id;
+    this.data = data;
+}
 
+Schedule.prototype = {
+    constructor: Schedule,
 
-!function (global) {
-  'use strict';
+    init: function () {
+        console.log("class", this.id, this.data);
+        const canvas = new fabric.Canvas(this.id);
+        this.basicSetUp(canvas);
+        this.setAxis(canvas, this.data);
+    },
 
-  var previousFancyInput = global.FancyInput;
+    basicSetUp: function (canvas) {
+        canvas.selection = false;
+        canvas.hoverCursor = 'pointer';
+    },
 
-  function FancyInput(options) {
-    this.options = options || {};
-  }
+    setAxis: function(canvas, data) {
+        const cWidth = canvas.width;
+        const cHeight = canvas.height;
+        const marginTop = 100; //顶部留白
+        const marginBottom = 100; //底部留白
+        const marginLeft = 50; //左侧留白
+        const buildLayers = data.randerData.buildLayers; //楼层数
+        const totalDays = this.toolFunc.calculateRangeDays(data.randerData.startDate, data.randerData.endDate); //预计楼栋建设持续天数
+        console.log(totalDays);
+        this.setAxisX(cWidth, cHeight, marginBottom, marginLeft, totalDays); //x轴是时间轴，需要总天数(预计楼栋建设持续时间加上左右预留时间)
+        this.setAxisY(cWidth, cHeight, marginTop, marginBottom, buildLayers); //y轴是楼层轴
+    },
 
-  FancyInput.noConflict = function noConflict() {
-    global.FancyInput = previousFancyInput;
-    return FancyInput;
-  };
-  
-  global.FancyInput = FancyInput;
+    setAxisX: function(cWidth,cHeight,marginBottom, marginLeft) {
+        console.log("canvas",canvas)
+        const lineX = this.toolFunc.makeLine([0, cHeight-marginBottom, cWidth, cHeight-marginBottom], '#09f');//x轴线
+        // X轴刻度
+        // for (let i = 1; i <= 15; i++) {
+        //     canvas.add(this.toolFunc.makeLine([marginLeft + 60 * i, cHeight-marginBottom, marginLeft + 60 * i, cHeight-marginBottom - 8], '#000'));
+        //     canvas.add(this.toolFunc.makeText(i + '月', {
+        //         left: 40 + 60 * i,
+        //         top: 510
+        //     }));
+        // }
+        canvas.add(lineX);
+    },
+    setAxisY: function() {},
 
-}(this);
+    toolFunc: {
+        makeLine: function (coords, color = 'red') {
+            return new fabric.Line(coords, {
+                stroke: color,
+                strokeWidth: 1,
+                selectable: false,
+                evented: false,
+            });
+        },
+        makeText: function (text, position) {
+            return new fabric.Text(text, {
+                left: position.left,
+                top: position.top,
+                fontSize: 10,
+                selectable: false
+            });
+        },
+        //计算并返回两个日期之间的相差的天数
+        calculateRangeDays: function (dateString1, dateString2) {
+            //获取起始时间的毫秒数
+            //其中dateString1.replace('/-/g','/')是将日期格式为yyyy-mm-dd转换成yyyy/mm/dd
+            //Date.parse()静态方法的返回值为1970年1月1日午时到当前字符串时间的毫秒数，返回值为整数
+            //如果传入的日期只包含年月日不包含时分秒，则默认取的毫秒数为yyyy/mm/dd 00:00:00
+            //取的是0时0分0秒的毫秒数，如果传入的是2015/07/03 12:20:12则取值为该时间点的毫秒数
+            console.log(dateString1, dateString2);
+            const startDate = Date.parse(dateString1.replace('/-/g', '/'));
+            const endDate = Date.parse(dateString2.replace('/-/g', '/'));
+            // console.log(startDate, endDate);
+            //因为不传时分秒的时候 默认取值到dateString2的0时0分0秒时的毫秒数，这样就不包含当前天数的毫秒数
+            const diffDate = (endDate - startDate);
+            //计算出两个日期字符串之间的相差的天数
+            //如果计算时要包含日期的当前天，就要加上一天
+            const days = parseInt(diffDate / (1 * 24 * 60 * 60 * 1000));
+            return days;
+        },
+
+        // 传入日期及天数，返回相加后的新日期
+        addDate: function(date, days) {
+            var newDate = new Date(date.replace('/-/g', '/'));
+            newDate.setDate(newDate.getDate() + days);
+            var month = newDate.getMonth() + 1;
+            var day = newDate.getDate();
+            var time = newDate.getFullYear() + "-" + month + "-" + day;
+            console.log(time);
+            return time;
+        },
+
+        // 获取该月份天数
+        mGetDate: function(year, month) {
+            var d = new Date(year, month, 0);
+            return d.getDate();
+        }
+    }
+}
 
 
 
@@ -28,26 +111,27 @@ canvas.hoverCursor = 'pointer';
 
 // 创建一个矩形对象
 const rect = new fabric.Rect({
-  left: 100,
-  top: 100,
-  fill: 'red',
-  width: 20,
-  height: 20,
-  selectable: false
+    left: 100,
+    top: 100,
+    fill: 'red',
+    width: 20,
+    height: 20,
+    selectable: false
 });
 
+//圆形对象
 const circle = new fabric.Circle({
-  radius: 20,
-  fill: 'green',
-  left: 100,
-  top: 100,
-  selectable: false
+    radius: 20,
+    fill: 'green',
+    left: 100,
+    top: 100,
+    selectable: false
 });
 
 // 动画
 rect.animate('left', '+=800', {
-  onChange: canvas.renderAll.bind(canvas),
-  duration: 4000,
+    onChange: canvas.renderAll.bind(canvas),
+    duration: 4000,
 });
 
 
@@ -55,147 +139,143 @@ rect.animate('left', '+=800', {
 // 自定义类型
 const LabeledCircle = fabric.util.createClass(fabric.Circle, {
 
-  type: 'labeledCircle',
+    type: 'labeledCircle',
 
-  initialize: function (options) {
-    options || (options = {});
+    initialize: function (options = {}) {
 
-    this.callSuper('initialize', options);
-    this.set('planName', options.planName || '');
-    this.set('planDate', options.planDate || '');
-    this.set({
-      radius: 5,
-      strokeWidth: 3,
-      stroke: options.stroke||'#0c0',
-      selectable: false
-    });
-  },
+        this.callSuper('initialize', options);
+        this.set('planName', options.planName || '');
+        this.set('planDate', options.planDate || '');
+        this.set({
+            radius: 5,
+            strokeWidth: 3,
+            stroke: options.stroke || '#0c0',
+            selectable: false
+        });
+    },
 
-  toObject: function () {
-    return fabric.util.object.extend(this.callSuper('toObject'), {
-      planName: this.get('planName'),
-      planDate: this.get('planDate'),
-    });
-  },
+    toObject: function () {
+        return fabric.util.object.extend(this.callSuper('toObject'), {
+            planName: this.get('planName'),
+            planDate: this.get('planDate'),
+        });
+    },
 
-  _render: function (ctx) {
-    this.callSuper('_render', ctx);
-    ctx.fillStyle = '#333';
-  }
+    _render: function (ctx) {
+        this.callSuper('_render', ctx);
+        ctx.fillStyle = '#333';
+    }
 });
 
 const labeledCircle = new LabeledCircle({
-  left: 500,
-  top: 200,
-  planName: '砌体工程',
-  planDate: '2018-12-19',
-  fill: '#faa',
-  stroke: 'red',
+    left: 500,
+    top: 200,
+    planName: '砌体工程',
+    planDate: '2018-12-19',
+    fill: '#faa',
+    stroke: 'red',
 });
 
 const labeledCircle2 = new LabeledCircle({
-  left: 430,
-  top: 200,
-  planName: '主体封顶',
-  planDate: '2018-12-19',
-  fill: '#06f'
+    left: 430,
+    top: 200,
+    planName: '主体封顶',
+    planDate: '2018-12-19',
+    fill: '#06f'
 });
 
 
 
-function makeLine(coords, color) {
-  return new fabric.Line(coords, {
-    stroke: color||'red',
-    strokeWidth: 1,
-    selectable: false,
-    evented: false,
-  });
+function makeLine(coords, color = 'red') {
+    return new fabric.Line(coords, {
+        stroke: color,
+        strokeWidth: 1,
+        selectable: false,
+        evented: false,
+    });
 }
 
 function makeText(text, position) {
-  return new fabric.Text(text, {
-    left: position.left,
-    top: position.top,
-    fontSize: 10,
-    selectable: false
-  });
+    return new fabric.Text(text, {
+        left: position.left,
+        top: position.top,
+        fontSize: 10,
+        selectable: false
+    });
 }
 
-const line = makeLine([ 250, 125, 250, 175 ]);
-const line2 = makeLine([ 250, 175, 250, 250 ]);
-const line3 = makeLine([ 250, 250, 300, 350]);
-
-const lineY = makeLine([ 100, 50, 100, 600 ], '#09f');
-const lineX = makeLine([ 0, 550, 1000, 550 ],'#09f');
+const lineY = makeLine([50, 100, 50, 600], '#09f');
+const lineX = makeLine([0, 500, 1000, 500], '#09f');
 
 // X轴刻度
-for (let i = 1; i < 15; i++) {
-  canvas.add(makeLine([ 100+60*i, 550, 100+60*i, 542 ], '#000'));
-  canvas.add(makeText(i + '月',{left: 90+60*i,top:560}));
+for (let i = 1; i <= 15; i++) {
+    canvas.add(makeLine([50 + 60 * i, 500, 50 + 60 * i, 492], '#000'));
+    canvas.add(makeText(i + '月', {
+        left: 40 + 60 * i,
+        top: 510
+    }));
 }
 
 // Y轴刻度
-for (let i = 1; i <= 20; i++) {
-  canvas.add(makeLine([100, 550 - 25 * i, 105, 550 - 25 * i], '#000'));
-  canvas.add(makeText(i + '', {
-    left: 80,
-    top: 550 - 25 * i
-  }));
+for (let i = 1; i <= 16; i++) {
+    canvas.add(makeLine([50, 500 - 25 * i, 55, 500 - 25 * i], '#000'));
+    canvas.add(makeText(i + '', {
+        left: 35,
+        top: 508 - 25 * i
+    }));
 }
 
 //原点
-canvas.add(makeText('0',{
-  left: 100,
-  top: 550
+canvas.add(makeText('0', {
+    left: 50,
+    top: 500
 }))
 
-var point = [100 + 157*(1.42),550-7*(25.23)];
+var point = [100 + 157 * (1.42), 550 - 7 * (25.23)];
 // 根据日期和楼层数自动转换为left与top
 
 // const everyDayLength = getEveryDayLength();
 // const everyFloorHeight = getEveryFloorHeight();
 
-function calculatePointCoord(date,level,everyDayLength,everyFloorHeight) {
-  // 根据日期和楼层数计算坐标
-  var days = getDays(date);
+function calculatePointCoord(date, level, everyDayLength, everyFloorHeight) {
+    // 根据日期和楼层数计算坐标
+    var days = getDays(date);
 
 }
 
 const labeledCircle3 = new LabeledCircle({
-  left: 300,
-  top: 200,
-  planName: '砌体工程',
-  planDate: '2018-12-19',
-  fill: '#ff9',
-  stroke: 'red',
+    left: 300,
+    top: 200,
+    planName: '砌体工程',
+    planDate: '2018-12-19',
+    fill: '#ff9',
+    stroke: 'red',
 });
 
 canvas.add(labeledCircle3)
 
 
 
-canvas.add(line, line2, line3,lineY,lineX);
+canvas.add(lineY, lineX);
 
 
-canvas.add(labeledCircle,labeledCircle2);
+canvas.add(labeledCircle, labeledCircle2);
 
 canvas.add(rect, circle);
 
-const arr = ['砌体工程','主体封顶']
+const arr = ['砌体工程', '主体封顶']
 
 canvas.on('mouse:down', function (options) {
-  console.log(options.e.clientX, options.e.clientY, options.target);
-  if (options.target) {
-    if (options.target.planName && arr.indexOf(options.target.planName) !== -1) {
-      console.log('sss',options.target.planName,options.target.planDate);
-      
-      options.target.animate('left', '+=50', {
-        onChange: canvas.renderAll.bind(canvas),
-      });
+    console.log(options.e.clientX, options.e.clientY, options.target);
+    if (options.target) {
+        if (options.target.planName && arr.indexOf(options.target.planName) !== -1) {
+            console.log('sss', options.target.planName, options.target.planDate);
 
+            options.target.animate('left', '+=50', {
+                onChange: canvas.renderAll.bind(canvas),
+            });
+
+        }
     }
-  }
 
 });
-
-
