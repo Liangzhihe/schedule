5e9f14bec185a1de9c789da20dcf1f4aff185877
Liangzhihe <1789211411@qq.com>
2019-02-25 16:20:59 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
-module.exports = {
-    "extends": "airbnb-base",
-    "env": {
-        "browser": true,
-        "node": true,
-        "jquery": true
-    },
-    "rules": {
-        "no-console": "off",
-        // "no-restricted-globals": "off",
-        "linebreak-style": ["error", "windows"],
-        "indent": ["error", 4]
-    }
-};
+// module.exports = {
+//     "extends": "airbnb-base",
+//     "env": {
+//         "browser": true,
+//         "node": true,
+//         "jquery": true
+//     },
+//     "rules": {
+//         "no-console": "off",
+//         // "no-restricted-globals": "off",
+//         "linebreak-style": ["error", "windows"],
+//         "indent": ["error", 4]
+//     }
+// };
